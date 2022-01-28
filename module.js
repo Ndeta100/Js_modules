dc769fa3e697469302dfb237783c70be5145e179
Ndeta Innocent <you@example.com>
2022-01-28 16:20:08 +0200

    revealing pattern

--- module.js
+++ module.js
@@ -11,4 +11,17 @@ fight(harry, voldemort)
 
 
 // THE MODULE PATTERN
- 
+var fightModile=(function (){
+    var harry='potter'
+    var voldemort='He who must not be named'
+    
+    function fight(char1, char2){
+        var attack1=Math.floor(Math.random()*char1.length)
+        var attack2=Math.floor(Math.random()*char2.length)
+        return attack1>attack2? `${char1} wins` : `${caches} wins`
+    }
+    
+   return {
+       fight:fight
+   }  
+})()
