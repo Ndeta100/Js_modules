f9cda3d70fa031e98e6e4e3341911a07d97771fe
Ndeta Innocent <you@example.com>
2022-01-28 16:46:43 +0200

    AMD

--- module.js
+++ module.js
@@ -14,7 +14,7 @@ fight(harry, voldemort)
 var fightModile=(function (){
     var harry='potter'
     var voldemort='He who must not be named'
-    
+
     function fight(char1, char2){
         var attack1=Math.floor(Math.random()*char1.length)
         var attack2=Math.floor(Math.random()*char2.length)
@@ -25,3 +25,30 @@ var fightModile=(function (){
        fight:fight
    }  
 })()
+
+
+// Common Js and AMD
+var module1=require('modules1')
+var modules2=require('module2')
+
+function fight(){
+
+}
+
+module1.exports={
+    fight:fight
+}
+
+// AMD
+define(['module1', 'module2'],
+function (moduleImport, moduleEXport){
+    var module1=moduleImport
+    var module2=moduleEXport
+    function dance (){
+
+    }
+    return {
+        dance:dance
+    }
+})
+
